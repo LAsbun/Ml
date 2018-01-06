21aa459380a7cf51a6d3dad5244a963c215fef0c
sws <Shengweisong@upg.cn>
2018-01-06 12:11:47 +0800

    1 理解k近邻分类算法，增加手写数据分类

--- MachineLearningAction/ch2/knn_date.py
+++ MachineLearningAction/ch2/knn_date.py
@@ -7,6 +7,11 @@
 @file: knn_date.py
 @time: 12/15/17 4:37 PM
 @desc:
+
+    所谓的knn（k近邻算法）：
+    本质上是根据一定的已标记数据，待分类数据与已标记数据计算所有的距离，
+    选其中距离最短的前n个，这前n个中分类出现最多的分类，就是待分类数据的分类
+
 """
 import os
 import operator
@@ -59,6 +64,7 @@ def classify0(inX, dataSet, labels, k):
 
     dataSetSize = dataSet.shape[0]
 
+    # tile（A, B) 重复A, B次   a, (b, c)  在行上重复b次，在列上重复c次
     diffMat = tile(inX, (dataSetSize, 1)) - dataSet
 
     sqDiffMat = diffMat ** 2
@@ -67,6 +73,7 @@ def classify0(inX, dataSet, labels, k):
 
     distince = sqDis ** 0.5
 
+    # argsort 返回从小到大的数据下标 a = array([3, 1, 2]) a.argsort()==> [1, 2, 0]
     sortedDis = distince.argsort()
 
     classCount = {}
@@ -74,6 +81,7 @@ def classify0(inX, dataSet, labels, k):
     for i in range(k):
         vateLabel = labels[sortedDis[i]]
         classCount[vateLabel] = classCount.get(vateLabel, 0) + 1
+    # classCount 是统计各种分类出现的额次数，最后按照从大到小排序，返回出现频率最高的那个
     sortedClassCount = sorted(classCount.items(), key=operator.itemgetter(1), reverse=True)
 
     return sortedClassCount[0][0]
@@ -131,7 +139,55 @@ def draw(file_name):
     plt.show()
 
 
+def img2vec(file_name):
+    """
+        将图片转化成向量
+    :param file_name:
+    :return: VEC
+    """
+
+    returnVec = zeros([1, 1024])
+
+    with open(file_name, "r") as f:
+        for i in range(32):
+            _line = f.readline()
+            for j in range(32):
+                returnVec[0, 32*i+j] = int(_line[j])
+    return returnVec
+
+def handwritingClassTest():
+
+    hwLabels = []
+
+    trainFileList = os.listdir('trainingDigits')
+
+    m = len(trainFileList)
+    trainMat = zeros((m, 1024))
+    i = 0
+    for tf in trainFileList:
+        hwLabels.append(tf.split('_')[0])
+        trainMat[i, :] = img2vec("trainingDigits/{}".format(tf))
+        i += 1
+
+    testFileList = os.listdir('testDigits')
+
+    error_count = 0
+    tm = len(testFileList)
+
+    for i in testFileList:
+        _tmp_vec = img2vec("testDigits/{}".format(i))
+        pre = classify0(_tmp_vec, trainMat, hwLabels, 3)
+        _tmp_label = i.split('_')[0]
+        print("预测: {}. 实际: {}".format(pre, _tmp_label))
+        if(pre != _tmp_label): error_count += 1
+
+    print("{} {}".format(error_count, tm))
+
 if __name__ == '__main__':
     # file_name = "datingTestSet2.txt"
     # draw(file_name)
-    datingClassTest()
+    # datingClassTest()
+    cur_path = os.getcwd()
+    print(img2vec('testDigits/0_0.txt')[0, 64:128])
+    print(os.listdir("testDigits"))
+    handwritingClassTest()
